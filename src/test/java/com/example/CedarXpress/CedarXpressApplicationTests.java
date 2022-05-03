95b27e6842d22298693c71cdc7bc17b7faf95c18
user <obidikewinifred@yahoo.com>
2022-05-03 11:58:54 +0100

    added boiler plate code

---
+++ src/test/java/com/example/CedarXpress/CedarXpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CedarXpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CedarXpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
