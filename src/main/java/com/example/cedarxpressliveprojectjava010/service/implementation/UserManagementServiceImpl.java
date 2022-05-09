354c3890bf5a9e7f632e04e78857a01658c2da88
ebube <chinekeebube@gmail.cm>
2022-05-09 12:22:21 +0100

    Resolved merge conflict

--- src/main/java/com/example/cedarxpressliveprojectjava010/service/implementation/UserManagementServiceImpl.java
+++ src/main/java/com/example/cedarxpressliveprojectjava010/service/implementation/UserManagementServiceImpl.java
@@ -1,48 +1,64 @@
 package com.example.cedarxpressliveprojectjava010.service.implementation;
 
+
 import com.example.cedarxpressliveprojectjava010.config.jwt.JwtTokenProvider;
 import com.example.cedarxpressliveprojectjava010.dto.request.ForgotPasswordRequest;
-import com.example.cedarxpressliveprojectjava010.dto.response.MessageResponse;
 import com.example.cedarxpressliveprojectjava010.dto.request.ResetPasswordRequest;
+import com.example.cedarxpressliveprojectjava010.dto.response.MessageResponse;
 import com.example.cedarxpressliveprojectjava010.entity.User;
 import com.example.cedarxpressliveprojectjava010.repository.UserRepository;
 import com.example.cedarxpressliveprojectjava010.service.UserManagementService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class UserManagementServiceImpl implements UserManagementService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
 
+    @Value("${app.base-url}")
+    private String passwordUrl;
+
     @Override
     public ResponseEntity<MessageResponse> forgotPassword(ForgotPasswordRequest request) {
         String email = request.getEmail();
         User user = userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new RuntimeException(
-                        String.format("User with email %s doesn't exist!", email)));
-        String JWToken = jwtTokenProvider.generateResetPasswordJWT(user);
-        String forgotPasswordURL = "http://localhost:8080/reset-password/" + JWToken;
+                .orElseThrow(() -> new NotFoundException(
+                        "User with email " + email + " doesn't exist!"));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user.getEmail(),
+                null,
+                Collections.singleton(new SimpleGrantedAuthority(String.valueOf(user.getRole())))
+        );
+        String JWToken = jwtTokenProvider.generateToken(authentication);
+        String forgotPasswordURL = passwordUrl + "/reset-password/" + JWToken;
 
-        System.out.println(forgotPasswordURL);
+        log.info(forgotPasswordURL);
         // send mail method;
-         return ResponseEntity.ok(new MessageResponse("Kindly check email for reset Link!"));
+        return ResponseEntity.ok(new MessageResponse("Kindly check email for reset Link!"));
     }
 
     @Override
     public ResponseEntity<MessageResponse> resetPassword(String jwToken, ResetPasswordRequest request) {
         String email = request.getEmail();
         User user = userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new RuntimeException(
+                .orElseThrow(() -> new NotFoundException(
                         String.format("User with email %s doesn't exist!", email)));
-        jwtTokenProvider.validateResetPasswordJWT(jwToken, user.getPassword());
+        jwtTokenProvider.validateToken(jwToken);
 
         if (!request.getNewPassword().equals(request.getConfirmPassword()))
             throw new RuntimeException("Password incorrect!");
@@ -52,6 +68,5 @@ public class UserManagementServiceImpl implements UserManagementService {
         userRepository.save(user);
 
         return ResponseEntity.ok(new MessageResponse("Password updated successfully"));
-
     }
 }
